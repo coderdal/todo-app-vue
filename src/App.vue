8f642e91990b02fc57d095800035b924ed97d6fb
coderdal <agircoder@gmail.com>
2022-07-04 17:22:05 +0300

    Header design

--- src/App.vue
+++ src/App.vue
@@ -1,29 +1,41 @@
 <template>
   <div id="app">
     
-
+    <AppHeader />
 
   </div>
 </template>
 
 <script>
 
+import AppHeader from "./components/appHeader.vue";
+
 
 
 export default {
   name: 'App',
   components: {
-    
+    AppHeader
 }
 }
 </script>
 
 <style>
 
+*{
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+}
+
+body{
+  background: #212d38;
+}
+
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  color: #212d38;
+  color: #fff;
 }
 </style>
---
+++ src/components/appHeader.vue
@@ -0,0 +1,39 @@
+<template>
+    
+    <header>
+        <h1>Todo App with</h1>
+        <h2><img src="../assets/logo.png" alt="vue icon" />ue JS</h2>
+    </header>
+
+</template>
+
+<script></script>
+
+<style>
+
+header{
+    width: 100%;
+    height: 75px;
+    text-align: center;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    flex-wrap: wrap;
+    gap: 10px;
+    padding: 5px 10px;
+    font-family: 'Trebuchet MS', 'Lucida Sans Unicode', 'Lucida Grande', 'Lucida Sans', Arial, sans-serif;
+}
+
+header img {
+    width: 30px;
+}
+
+header h2 {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    font-family:'Lucida Sans', 'Lucida Sans Regular', 'Lucida Grande', 'Lucida Sans Unicode', Geneva, Verdana, sans-serif;
+}
+
+
+</style>
