b14b7ff38e971bb9f3173164a74f8178084333e2
coderdal <agircoder@gmail.com>
2022-07-04 17:10:17 +0300

    Initial

---
+++ src/App.vue
@@ -0,0 +1,29 @@
+<template>
+  <div id="app">
+    
+
+
+  </div>
+</template>
+
+<script>
+
+
+
+export default {
+  name: 'App',
+  components: {
+    
+}
+}
+</script>
+
+<style>
+
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  color: #212d38;
+}
+</style>
